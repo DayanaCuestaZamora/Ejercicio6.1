7574b31fddb30ff77e86b8f985a5d8c06d55776e
DayanaCuestaZamora <cuesta.zamora@gmail.com>
2016-02-01 13:45:06 -0500

    Merge origin/master
    Conflicts:
    	ActividadGrupo/src/actividadgrupo/ActividadGrupo.java
    Dayana

--- ActividadGrupo/src/actividadgrupo/ActividadGrupo.java
+++ ActividadGrupo/src/actividadgrupo/ActividadGrupo.java
@@ -15,24 +15,27 @@ public class ActividadGrupo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Dayana cuesta zamora");
-        Memoria objeto = new Memoria();
-        objeto.setMarca("SanDisk");
-        System.out.println(objeto.getMarca());
-    }
+        
+    
         System.out.println("lina marcela barrera garcia");
         Anime objeto = new Anime();
         objeto.setNombre("Noragami");
         System.out.println("Nombre" + "  : " + objeto.getNombre());
-        Sunami objeto = new Sunami();
+        
+        Sunami objeto1 = new Sunami();
         System.out.println("kevin rincon mendez");
-          objeto.setNombre("soacha");
-        System.out.println(objeto.getNombre());
+        objeto1.setNombre("soacha");
+        System.out.println(objeto1.getNombre());
     
         System.out.println("johan narvaez gonzalez ");
-        Celular objeto = new Celular();
-        objeto.setMarca("ALCATEL");
-        System.out.println(objeto.getMarca());
+        Celular objeto2 = new Celular();
+        objeto2.setMarca("ALCATEL");
+        System.out.println(objeto2.getMarca());
+        
+           System.out.println("Dayana cuesta zamora");
+        Memoria objeto3 = new Memoria();
+        objeto3.setMarca("SanDisk");
+        System.out.println(objeto3.getMarca());
     }
 
 }
